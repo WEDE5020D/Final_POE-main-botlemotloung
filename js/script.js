0a92583a3a201ce2db64a1b6f19ffdbbe9dd2686
botlemotloung <botlemotloung07@gmail.com>
2026-06-19 10:30:51 +0200

    added Faq and a map Part 3

--- js/script.js
+++ js/script.js
@@ -230,3 +230,15 @@ function initGalleryLightbox() {
         }
     });
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach(q => {
+        q.addEventListener("click", () => {
+            const answer = q.nextElementSibling;
+
+            answer.style.display =
+                answer.style.display === "block" ? "none" : "block";
+        });
+    });
+});
